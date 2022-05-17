af8325e656101d44b528c8883af94e85aa2b1f4f
Antonio Román <kyradiscord@gmail.com>
2022-05-17 15:35:21 +0200

    chore: it compiles! ship it!

--- src/commands/suggest.ts
+++ src/commands/suggest.ts
@@ -1,11 +1,14 @@
-import { SuggestionStatusColors } from '#lib/common/constants';
+import { ButtonValue, CustomId, SuggestionStatusColors } from '#lib/common/constants';
 import { LanguageKeys } from '#lib/i18n/LanguageKeys';
 import { applyNameAndDescription } from '#lib/utilities/add-builder-localizations';
+import { getUser } from '#lib/utilities/interactions';
+import { getMessage, patchMessage, postMessage } from '#lib/utilities/rest';
 import { EmbedBuilder, time, userMention } from '@discordjs/builders';
 import type { Guild } from '@prisma/client';
+import { fromAsync } from '@sapphire/result';
 import { Command, RegisterCommand } from '@skyra/http-framework';
 import { getSupportedLanguageName, getT, resolveKey, resolveUserKey } from '@skyra/http-framework-i18n';
-import { ButtonStyle, ComponentType, MessageFlags, RESTPostAPIChannelMessageResult, Routes } from 'discord-api-types/v10';
+import { ButtonStyle, ComponentType, MessageFlags } from 'discord-api-types/v10';
 
 type MessageData = LanguageKeys.Commands.Suggestions.MessageData;
 
@@ -40,19 +43,18 @@ export class UserCommand extends Command {
 		const id = count + 1;
 		const user = this.makeUserData(interaction);
 		const body = this.makeMessage(interaction, settings, { id, message: input, timestamp: time(), user });
-		const message = (await this.container.rest.post(Routes.channelMessages(settings.channel.toString()), {
-			body
-		})) as RESTPostAPIChannelMessageResult;
+		const message = await postMessage(settings.channel, body);
 
 		await this.container.prisma.suggestion.create({
 			data: { id, guildId, authorId: BigInt(user.id), messageId: BigInt(message.id) }
 		});
 
-		return this.message({ content: `TODO: ${input}` });
+		const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestNewSuccess, { id });
+		return this.message({ content, flags: MessageFlags.Ephemeral });
 	}
 
 	private makeUserData(interaction: Command.Interaction): MessageData['user'] {
-		const user = (interaction.member?.user ?? interaction.user)!;
+		const user = getUser(interaction);
 
 		return {
 			id: user.id,
@@ -62,9 +64,9 @@ export class UserCommand extends Command {
 		};
 	}
 
-	private makeMessage(interaction: Command.Interaction, settings: Guild, data: MessageData) {
+	private makeMessage(interaction: Command.Interaction, settings: Guild, data: MessageData): postMessage.Body {
 		const resolved = settings.useEmbed ? this.makeEmbedMessage(interaction, data) : this.makeContentMessage(interaction, data);
-		return this.message({ ...resolved, components: this.makeComponents(interaction, settings) });
+		return { ...resolved, components: this.makeComponents(interaction, settings) };
 	}
 
 	private makeComponents(interaction: Command.Interaction, settings: Guild) {
@@ -79,7 +81,7 @@ export class UserCommand extends Command {
 			components: [
 				{
 					type: ComponentType.Button,
-					custom_id: 'suggestions:archive',
+					custom_id: CustomId.SuggestionsArchive,
 					style: ButtonStyle.Danger,
 					label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsArchive)
 				}
@@ -88,7 +90,7 @@ export class UserCommand extends Command {
 		if (!settings.addThread) {
 			manageRow.components.unshift({
 				type: ComponentType.Button,
-				custom_id: 'suggestions:thread',
+				custom_id: CustomId.SuggestionsThread,
 				style: ButtonStyle.Primary,
 				label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsCreateThread)
 			});
@@ -99,11 +101,11 @@ export class UserCommand extends Command {
 			components: [
 				{
 					type: ComponentType.SelectMenu,
-					custom_id: 'suggestions:resolve',
+					custom_id: CustomId.SuggestionsResolve,
 					options: [
-						{ label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsAccept), value: 'accept' },
-						{ label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsConsider), value: 'consider' },
-						{ label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsDeny), value: 'deny' }
+						{ label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsAccept), value: ButtonValue.SuggestionResolveAccept },
+						{ label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsConsider), value: ButtonValue.SuggestionResolveConsider },
+						{ label: t(LanguageKeys.Commands.Suggestions.SuggestComponentsDeny), value: ButtonValue.SuggestionResolveDeny }
 					]
 				}
 			]
@@ -112,15 +114,15 @@ export class UserCommand extends Command {
 		return components;
 	}
 
-	private makeEmbedMessage(interaction: Command.Interaction, data: MessageData) {
+	private makeEmbedMessage(interaction: Command.Interaction, data: MessageData): postMessage.Body {
 		const title = resolveKey(interaction, LanguageKeys.Commands.Suggestions.SuggestNewMessageEmbedTitle, data);
 		const embed = new EmbedBuilder().setColor(SuggestionStatusColors.Unresolved).setTitle(title).setDescription(data.message);
-		return this.message({ embeds: [embed.toJSON()] });
+		return { embeds: [embed.toJSON()] };
 	}
 
-	private makeContentMessage(interaction: Command.Interaction, data: MessageData) {
+	private makeContentMessage(interaction: Command.Interaction, data: MessageData): postMessage.Body {
 		const content = resolveKey(interaction, LanguageKeys.Commands.Suggestions.SuggestNewMessageContent, data);
-		return this.message({ content });
+		return { content };
 	}
 
 	private async handleEdit(interaction: Command.Interaction, id: number, input: string) {
@@ -129,22 +131,66 @@ export class UserCommand extends Command {
 			where: { id_guildId: { id, guildId } }
 		});
 
+		// If the suggestion does not exist, return early:
 		if (suggestion === null) {
 			const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestModifyDoesNotExist);
 			return this.message({ content, flags: MessageFlags.Ephemeral });
 		}
 
+		// If the suggestion was made by a different author, return early:
+		const userId = BigInt(getUser(interaction).id);
+		if (suggestion.authorId !== userId) {
+			const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestModifyMismatchingAuthor);
+			return this.message({ content, flags: MessageFlags.Ephemeral });
+		}
+
+		// If the suggestion was archived, return early:
 		if (suggestion.archivedAt !== null) {
 			const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestModifyArchived);
 			return this.message({ content, flags: MessageFlags.Ephemeral });
 		}
 
+		// If the suggestion was already replied to, its contents become immutable to avoid changing the contents after
+		// a decision. As such, return early:
 		if (suggestion.repliedAt !== null) {
 			const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestModifyReplied);
 			return this.message({ content, flags: MessageFlags.Ephemeral });
 		}
 
-		return this.message({ content: `TODO: ${input}` });
+		// Get the guild settings to get the channel:
+		const settings = await this.container.prisma.guild.findUnique({
+			where: { id: guildId },
+			select: { channel: true }
+		});
+
+		// If the settings were deleted or the channel not configured, everything becomes readonly. As such, return early:
+		if (!settings?.channel) {
+			const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestNewNotConfigured);
+			return this.message({ content, flags: MessageFlags.Ephemeral });
+		}
+
+		const result = await fromAsync(getMessage(settings.channel, suggestion.messageId));
+		if (!result.success) {
+			await this.container.prisma.suggestion.update({
+				where: { id_guildId: suggestion },
+				data: { archivedAt: new Date() }
+			});
+
+			const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestModifyMessageDeleted);
+			return this.message({ content, flags: MessageFlags.Ephemeral });
+		}
+
+		const message = result.value;
+		let data: patchMessage.Body;
+		if (message.embeds.length) {
+			data = { embeds: [{ ...message.embeds[0], description: input }] };
+		} else {
+			data = { content: message.content.slice(0, message.content.indexOf('\n')) + input };
+		}
+		await patchMessage(message.channel_id, message.id, data);
+
+		const content = resolveUserKey(interaction, LanguageKeys.Commands.Suggestions.SuggestModifySuccess, { id });
+		return this.message({ content, flags: MessageFlags.Ephemeral });
 	}
 }
 
--- src/lib/i18n/LanguageKeys/Commands/Suggestions.ts
+++ src/lib/i18n/LanguageKeys/Commands/Suggestions.ts
@@ -1,19 +1,24 @@
 import { FT, T } from '@skyra/http-framework-i18n';
 
 export const Suggest = 'commands:suggestions/suggest';
-export const SuggestOptionsSuggestion = 'commands:suggestions/suggestOptionsSuggestion';
-export const SuggestOptionsId = 'commands:suggestions/suggestOptionsId';
-export const SuggestModifyDoesNotExist = T('commands:suggestions/suggestModifyDoesNotExist');
+export const SuggestComponentsAccept = T('commands:suggestions/suggestComponentsAccept');
+export const SuggestComponentsArchive = T('commands:suggestions/suggestComponentsArchive');
+export const SuggestComponentsConsider = T('commands:suggestions/suggestComponentsConsider');
+export const SuggestComponentsCreateThread = T('commands:suggestions/suggestComponentsCreateThread');
+export const SuggestComponentsDeny = T('commands:suggestions/suggestComponentsDeny');
 export const SuggestModifyArchived = T('commands/suggestions/suggestModifyArchived');
+export const SuggestModifyDoesNotExist = T('commands:suggestions/suggestModifyDoesNotExist');
+export const SuggestModifyMessageDeleted = T('commands/suggestions/suggestModifyMessageDeleted');
+export const SuggestModifyMismatchingAuthor = T('commands/suggestions/suggestModifyMismatchingAuthor');
+export const SuggestModifyNotConfigured = T('commands/suggestions/suggestModifyNotConfigured');
 export const SuggestModifyReplied = T('commands/suggestions/suggestModifyReplied');
-export const SuggestNewNotConfigured = T('commands:suggestions/suggestNewNotConfigured');
+export const SuggestModifySuccess = FT<{ id: number }>('commands/suggestions/suggestModifySuccess');
 export const SuggestNewMessageContent = FT<MessageData>('commands:suggestions/suggestNewMessageContent');
 export const SuggestNewMessageEmbedTitle = FT<MessageData>('commands:suggestions/suggestNewMessageEmbedTitle');
-export const SuggestComponentsAccept = T('commands:suggestions/suggestComponentsAccept');
-export const SuggestComponentsConsider = T('commands:suggestions/suggestComponentsConsider');
-export const SuggestComponentsDeny = T('commands:suggestions/suggestComponentsDeny');
-export const SuggestComponentsCreateThread = T('commands:suggestions/suggestComponentsCreateThread');
-export const SuggestComponentsArchive = T('commands:suggestions/suggestComponentsArchive');
+export const SuggestNewNotConfigured = T('commands:suggestions/suggestNewNotConfigured');
+export const SuggestNewSuccess = FT<{ id: number }>('commands:suggestions/suggestNewSuccess');
+export const SuggestOptionsId = 'commands:suggestions/suggestOptionsId';
+export const SuggestOptionsSuggestion = 'commands:suggestions/suggestOptionsSuggestion';
 
 export interface MessageData {
 	id: number;
