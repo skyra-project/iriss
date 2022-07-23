c01dbc33e7885ade7cb78ef7e018c723d2e08307
Jeroen Claassens <support@favware.tech>
2022-07-23 17:26:22 +0200

    chore: crap. tonight is raid night and i am already late.
    permalink: http://whatthecommit.com/66f576e424e2eb1fc4f89b74192e3f1b

--- src/lib/utilities/message.ts
+++ src/lib/utilities/message.ts
@@ -1,15 +1,5 @@
 import type { Snowflake } from './rest';
 
-// export function url(message: APIMessage): ReturnType<typeof urlFromParams>;
-// export function url(guildId: Snowflake | '@me', channelId: Snowflake, messageId: Snowflake): ReturnType<typeof urlFromParams>;
 export function url(guildId: Snowflake | '@me', channelId: Snowflake, messageId: Snowflake) {
 	return `https://discord.com/channels/${guildId.toString()}/${channelId.toString()}/${messageId.toString()}` as const;
 }
-
-// function urlFromPayload(message: APIMessage) {
-// 	return urlFromParams(message?.guild_id ?? '@me', message.channel_id, message.id);
-// }
-
-// function urlFromParams(guildId: Snowflake | '@me', channelId: Snowflake, messageId: Snowflake) {
-// 	return `https://discord.com/channels/${guildId.toString()}/${channelId.toString()}/${messageId.toString()}` as const;
-// }
