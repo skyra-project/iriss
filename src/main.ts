4cab43560343cd2d7619eab5b8386718a0994fa5
Aura <kyradiscord@gmail.com>
2023-12-28 00:01:37 +0100

    refactor: update to HFx v2 (#200)
    Co-authored-by: Jeroen Claassens <support@favware.tech>

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,9 @@
 import { setup } from '#lib/setup/all';
 import { setupAPI } from '#lib/setup/api';
-import { registerCommands } from '#lib/utilities/register-commands';
 import { envParseInteger, envParseString } from '@skyra/env-utilities';
 import { Client, container } from '@skyra/http-framework';
 import { init, load } from '@skyra/http-framework-i18n';
+import { registerCommands } from '@skyra/shared-http-pieces';
 import { createBanner } from '@skyra/start-banner';
 import gradient from 'gradient-string';
 
